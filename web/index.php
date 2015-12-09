30d32536cd472eaa0c2d1e87abc3c330a1c24eb2
Sabine Miehlbradt <sabine.miehlbradt@scrutinizer-ci.com>
2015-12-09 11:27:31 +0100

    added route

--- web/index.php
+++ web/index.php
@@ -26,4 +26,9 @@ $app->get('/cowsay', function() use($app) {
   $app['monolog']->addDebug('cowsay');
   return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
 });
+
+$app->get('/test', function() use($app) {
+    $app['monolog']->addDebug('logging output.');
+    return str_repeat('Hello', 12);
+});
 $app->run();
